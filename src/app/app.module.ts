b03b70fbeadf967b86f920d2d5758c3f0f2d1b36
Giacomo Corallini <corallinigiacomo89@gmail.com>
2021-02-17 12:53:31 +0100

    favicon + badge on critical column

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
+import { BadgeModule } from 'primeng/badge';
+import { ButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,8 @@ import { ButtonModule } from 'primeng/button';
     TableModule,
     InputTextModule,
     ButtonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    BadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
