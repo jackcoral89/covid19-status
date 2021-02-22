b59873104af7867ca5feff80f1449908aec5f15f
Giacomo Corallini <corallinigiacomo89@gmail.com>
2021-02-22 19:13:46 +0100

    add skeleton component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { TableModule } from 'primeng/table';
 import { InputTextModule } from 'primeng/inputtext';
 import { BadgeModule } from 'primeng/badge';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
+import { CardModule, } from 'primeng/card';
+import { SkeletonModule } from 'primeng/skeleton';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ItalyComponent } from './italy/italy.component';
-import { FormsModule } from '@angular/forms';
-import { CardModule, } from 'primeng/card';
 import { CountryDetailComponent } from './country-detail/country-detail.component';
 
 @NgModule({
@@ -35,7 +36,8 @@ import { CountryDetailComponent } from './country-detail/country-detail.componen
     BadgeModule,
     MenubarModule,
     FormsModule,
-    CardModule
+    CardModule,
+    SkeletonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://corona.lmao.ninja/v2/countries'
+  API_URL: 'https://disease.sh/'
 };
